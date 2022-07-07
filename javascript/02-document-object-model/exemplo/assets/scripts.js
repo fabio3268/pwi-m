11605f9912c7c1572fb1e34ed73423ecffa6e2ba
Fábio Santos <fabio3268@gmail.com>
2022-07-07 08:37:11 -0300

    Semana 06

---
+++ javascript/02-document-object-model/exemplo/assets/scripts.js
@@ -0,0 +1,12 @@
+let area = document.querySelector("#area");
+
+function Clicar(){
+    area.innerHTML = "<strong>Clicou!</strong>";
+}
+function Entrar (){
+    area.innerHTML = "<strong>Mouse entrou na DIV!</strong>";
+}
+
+function Sair(){
+    area.innerHTML = "<strong>Mouse saiu da DIV</strong>";
+}

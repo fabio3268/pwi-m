de4743a2b826bc4f0096c4a8ce528fbfea93c27e
Fábio Santos <fabio3268@gmail.com>
2023-04-18 11:44:25 -0300

    Arrays Matrizes e Objetos

---
+++ javascript/02-eventos/lista-compras/assets/scripts.js
@@ -0,0 +1,17 @@
+
+
+
+
+const item = document.querySelector("#item");
+const myButtonInsert = document.querySelector("#myButtonInsert");
+const myList = document.querySelector("#myList");
+
+function insertItem(){
+
+    const newItem = document.createElement("li");
+    newItem.textContent = item.value;
+    console.log(newItem);
+    myList.insertAdjacentElement("afterbegin",newItem);
+}
+
+myButtonInsert.addEventListener("click", insertItem);

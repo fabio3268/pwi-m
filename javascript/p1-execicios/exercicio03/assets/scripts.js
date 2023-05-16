b38f7c76dd2e182c0c0fe849b6c0106b57fc4bb9
Fábio Santos <fabio3268@gmail.com>
2023-05-16 05:26:35 -0300

    Exercícios para Prova

--- javascript/p1-execicios/exercicio03/assets/scripts.js
+++ javascript/p1-execicios/exercicio03/assets/scripts.js
@@ -1,1 +1,19 @@
 const arrayFruits = ['batata','laranja','morango','abacaxi'];
+
+const inputFruit = document.querySelector("#fruit");
+const button = document.querySelector("button");
+
+function search (e) {
+    console.log(e);
+    if(inputFruit.value === e){
+        document.querySelector(".resultSearch").textContent = "Encontrada";
+    }
+}
+
+function searchFruit () {
+    document.querySelector(".resultSearch").textContent = "Não Encontrada";
+    arrayFruits.forEach(search);
+}
+
+button.addEventListener("click",searchFruit);
+

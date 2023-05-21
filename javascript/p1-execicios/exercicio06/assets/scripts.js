de67a0023ea64203afb3685b345e23e23c88f631
Fábio Santos <fabio3268@gmail.com>
2023-05-21 19:36:05 -0300

    Exercícios para Prova

--- javascript/p1-execicios/exercicio06/assets/scripts.js
+++ javascript/p1-execicios/exercicio06/assets/scripts.js
@@ -1,24 +1,2 @@
-const usersList = [
-    {
-        name : "Fábio Santos",
-        email : "fabio@example.com",
-        password : "12345"
-    },
-    {
-        name: "Maria Oliveira",
-        email: "maria@example.com",
-        password: "12345"
-    },
-    {
-        name: "Carlos Ferreira",
-        email: "carlos@example.com",
-        password: "12345"
-    },
-    {
-        name: "João da Silva",
-        email: "joao@example.com",
-        password: "12345"
-    }
-];
+const vetAges = [];
 
-console.log(usersList);

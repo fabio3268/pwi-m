8e48cae5710fa568d0a9be46a2704ec3111a596a
Fábio Santos <fabio3268@gmail.com>
2023-05-23 16:22:38 -0300

    Exercícios para Prova

--- javascript/p1-execicios/exercicio13/assets/scripts.js
+++ javascript/p1-execicios/exercicio13/assets/scripts.js
@@ -53,20 +53,42 @@ const tasksList = [
 
 const divTasks = document.querySelector("#task-list");
 
-for (let i in tasksList){
-    console.log(tasksList[i].description, tasksList[i].status);
-    let divTask = document.createElement("div");
-    divTask.setAttribute("class","task");
-    divTask.innerHTML = `
+function listTaskCreator (){
+    for (let i in tasksList){
+        //console.log(tasksList[i].description, tasksList[i].status);
+        let divTask = document.createElement("div");
+        divTask.setAttribute("class","task");
+        divTask.innerHTML = `
         <span class="status">${tasksList[i].status}</span>
         <span class="description">${tasksList[i].description}</span>
     `;
-    let buttonDelete = document.createElement("button");
-    buttonDelete.setAttribute("class","delete-btn");
-    divTasks.insertAdjacentElement("beforeend",divTask);
-    buttonDelete.textContent = "Excluir";
-    buttonDelete.addEventListener("click", () => {
-        buttonDelete.parentElement.remove();
-    });
-    divTask.insertAdjacentElement("beforeend",buttonDelete);
+        let buttonDelete = document.createElement("button");
+        buttonDelete.setAttribute("class","delete-btn");
+        divTasks.insertAdjacentElement("beforeend",divTask);
+        buttonDelete.textContent = "Excluir";
+        buttonDelete.addEventListener("click", () => {
+            buttonDelete.parentElement.remove();
+        });
+        divTask.insertAdjacentElement("beforeend",buttonDelete);
+    }
 }
+
+listTaskCreator();
+
+const buttonInsert = document.querySelector("#add-task-btn");
+
+function insertTask (){
+    const description = document.querySelector("#task-description");
+    const status = document.querySelector("#task-status");
+    let taskObj = {
+        description: description.value,
+        status: status.value,
+        creationDate: "26/05"
+    };
+    tasksList.push(taskObj);
+    divTasks.innerHTML = "";
+    listTaskCreator();
+    console.table(tasksList);
+}
+
+buttonInsert.addEventListener("click",insertTask);

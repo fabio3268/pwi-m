b3f4b1039da95c61a35273fc1d031c2005007c0c
Fábio Santos <fabio3268@gmail.com>
2023-06-20 11:08:16 -0300

    Exemplos - com css

--- javascript/04-armazenamento-local/exemplo03/assets/scripts.js
+++ javascript/04-armazenamento-local/exemplo03/assets/scripts.js
@@ -1,3 +1,7 @@
+
+
+
+
 const body = document.querySelector("body");
 const btnAdd = document.querySelector('#button-add');
 const inputItem = document.querySelector('#input-item');
@@ -7,8 +11,43 @@ body.addEventListener('click', (event) => {
     console.log("Oi do Body");
 });
 
+inputItem.addEventListener("click", (event) => {
+    event.stopPropagation();
+});
+
 btnAdd.addEventListener('click', (event) => {
     console.log("Oi do Button");
+    event.stopPropagation();
+});
+
+document.querySelector("#div01").addEventListener("click", () => {
+    console.log("Oi div 01");
 });
 
 
+
+
+
+/*
+inputItem.addEventListener('click', (event) => {
+   event.stopPropagation();
+});
+
+btnAdd.addEventListener('click', (event) => {
+    if(event.target.tagName === "BUTTON"){
+        const liNewItem = document.createElement("li");
+        liNewItem.textContent = inputItem.value;
+        ulItems.insertAdjacentElement("beforeend", liNewItem);
+    }
+    event.stopPropagation();
+});
+
+ulItems.addEventListener('click', (event) => {
+    if(event.target.tagName === "LI"){
+        event.target.remove();
+    }
+    //event.stopPropagation();
+    //console.log("Oi da UL");
+});
+
+ */

406afa5fd7868f683542224c78d2bfe7dadd1061
Fábio Santos <fabio3268@gmail.com>
2023-06-23 11:01:23 -0300

    Exemplos - com css

--- javascript/01-document-object-model/jogo-vellha/assets/scripts.js
+++ javascript/01-document-object-model/jogo-vellha/assets/scripts.js
@@ -1,1 +1,10 @@
+const board = document.querySelector("#board");
+let play = "X";
 
+board.addEventListener("click", (event) => {
+    console.log(event.target);
+    if(event.target.classList.contains("cell")){
+        event.target.textContent = play;
+        play = play === "X" ? "O" : "X";
+    }
+});

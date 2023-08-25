30ad2f0146e24c16021241ae1f97e47c8d4e6167
Fábio Santos <fabio3268@gmail.com>
2023-08-25 07:26:31 -0300

    JS - PHP

---
+++ js-php/01-exemplo/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemplo 01 - Olá Mundo</title>
+    <link rel="stylesheet" href="assets/styles.css">
+    <script type="text/javascript" src="assets/scripts.js" async></script>
+</head>
+<body>
+
+<button>Olá, Mundo!</button>
+
+</body>
+</html>

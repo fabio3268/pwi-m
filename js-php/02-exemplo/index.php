50b2dbb15de20e9f379f54667ef4f691b6df3dba
Fábio Santos <fabio3268@gmail.com>
2023-08-31 07:29:25 -0300

    Programação Assíncrona

--- js-php/02-exemplo/index.php
+++ js-php/02-exemplo/index.php
@@ -2,11 +2,13 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Exemplo 01</title>
+    <title>Exemplo 02 - Olá Mundo</title>
     <link rel="stylesheet" href="assets/styles.css">
     <script type="text/javascript" src="assets/scripts.js" async></script>
 </head>
 <body>
+
+<button>Olá, Mundo!</button>
 
 </body>
 </html>
--- js-php/03-exemplo/index.php
+++ js-php/03-exemplo/index.php
@@ -2,11 +2,12 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Exemplo 01</title>
+    <title>Exemplo 03 - Pessoa</title>
     <link rel="stylesheet" href="assets/styles.css">
     <script type="text/javascript" src="assets/scripts.js" async></script>
 </head>
 <body>
+
 
 </body>
 </html>

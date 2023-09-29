62945ca634f6495b7bcba6da94d2b24a86d4a8ec
Fábio Santos <fabio3268@gmail.com>
2023-09-29 08:26:40 -0300

    Banco de Dados

--- js-php/exercicios/03-exercicio/api/connection.php
+++ js-php/exercicios/03-exercicio/api/connection.php
@@ -5,17 +5,24 @@ $user = "root";
 $password = "12345";
 $database = "db_company_3am";
 $port = "3306";
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
 
 $conn = new PDO(
     "mysql:host=$host;port=$port;dbname=$database",
     $user,
-    $password
+    $password,
+    $options
 );
 
-var_dump($conn);
-
-$stmt = $conn->query("SELECT * FROM products");
-
-var_dump($stmt->fetchAll());
+//var_dump($conn);
+//
+//$stmt = $conn->query("SELECT * FROM products");
+//
+//$response = $stmt->fetchAll();
+//
+//var_dump($response);
 
 

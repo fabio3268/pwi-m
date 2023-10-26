aa1216ad6182b01765f6f334f70db446256b4ed1
Fábio Santos <fabiosantos@ifsul.edu.br>
2023-10-26 06:50:24 -0300

    Exercícios 05

--- js-php/exercicios/04-exercicio/api/connection.php
+++ js-php/exercicios/04-exercicio/api/connection.php
@@ -2,9 +2,9 @@
 
 $host = "localhost";
 $user = "root";
-$password = "12345"; // trocar a senha
-$database = "db_company_3am";
-$port = 3306;
+$password = "asdf1234"; // trocar a senha
+$database = "db_book_store_manha";
+$port = 3310;
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,

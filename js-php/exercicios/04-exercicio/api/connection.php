c019b006d7c844455f18d2f6ee3a581436b5f7ba
Fábio Santos <fabiosantos@ifsul.edu.br>
2023-11-01 06:14:31 -0300

    Atualização

--- js-php/exercicios/04-exercicio/api/connection.php
+++ js-php/exercicios/04-exercicio/api/connection.php
@@ -1,28 +1,17 @@
 <?php
 
-$host = "mysql";
+$host = "mysql"; // na escola ou em casa, usar localhost
 $user = "root";
 $password = "asdf1234"; // trocar a senha
-$database = "db_book_store_manha";
+$database = "db_company_2am";
 $port = 3306;
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
-$conn = new PDO(
-    "mysql:host=$host;port=$port;dbname=$database",
-    $user,
-    $password,
-    $options
-);
-
-//var_dump($conn);
-
-//$query = "SELECT * FROM categories";
-//$stmt = $conn->query($query);
-
-//var_dump($stmt->fetchAll());
-
-///echo json_encode($stmt->fetchAll(), JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$database", $user, $password, $options);
+} catch (PDOException $e) {
+    echo "Erro de conexão: " . $e->getMessage();
+}

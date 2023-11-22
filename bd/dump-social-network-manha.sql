9b671b21ba467bfd01df1f81bb9789b066ab510f
Fábio Santos <fabiosantos@ifsul.edu.br>
2023-11-22 17:20:58 -0300

    Exemplo 06 - Upload de arquivos

---
+++ bd/dump-social-network-manha.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: social_network
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE SCHEMA IF NOT EXISTS `social_network_manha`;
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+                              `id` int NOT NULL AUTO_INCREMENT,
+                              `name` varchar(255) NOT NULL,
+                              PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+INSERT INTO `categories` VALUES (1,'Amigos'),(2,'Família'),(3,'Trabalho');
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+                            `id` int NOT NULL AUTO_INCREMENT,
+                            `content` varchar(255) NOT NULL,
+                            `post_id` int NOT NULL,
+                            PRIMARY KEY (`id`),
+                            KEY `fk_comments_posts1_idx` (`post_id`),
+                            CONSTRAINT `fk_comments_posts1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `content` varchar(255) NOT NULL,
+                         `category_id` int NOT NULL,
+                         `user_id` int NOT NULL,
+                         PRIMARY KEY (`id`),
+                         KEY `fk_posts_categories_idx` (`category_id`),
+                         KEY `fk_posts_users1_idx` (`user_id`),
+                         CONSTRAINT `fk_posts_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+                         CONSTRAINT `fk_posts_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `name` varchar(255) NOT NULL,
+                         `email` varchar(255) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `image` varchar(255) DEFAULT NULL,
+                         PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'John Doe','john.doe@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(2,'Jane Smith','jane.smith@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(3,'Alice Johnson','alice.j@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(4,'Bob Anderson','bob.anderson@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(5,'Eva Williams','eva.w@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(6,'Daniel Lee','daniel.lee@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(7,'Sophia Brown','sophia.b@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(8,'David Miller','david.m@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(9,'Olivia Davis','olivia.d@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL),(10,'Michael White','michael.w@example.com','$2y$10$erjR7RQMmDObQaWisjoOvuOAsuUa2yfLnA5yF.LkBekUSMidKheve',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-11-21 14:45:51
--- bd/dump-social-network-tarde.sql
+++ bd/dump-social-network-tarde.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `categories`
 --
 
+CREATE SCHEMA IF NOT EXISTS `social_network_tarde`;
+
 DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;

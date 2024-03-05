02140a5af06fa66935b59a4a07891e58e3b5b999
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-05 07:29:31 -0300

    querySelector

--- javascript/01-dom/exemplo01/assets/scripts.js
+++ javascript/01-dom/exemplo01/assets/scripts.js
@@ -1,1 +1,10 @@
+//window.alert("Oi");
 
+
+
+document.body.style.background = "pink";
+document.body.style.fontFamily = "cursive";
+document.querySelector("h1").innerHTML = "Olá, INF-2AM!";
+document.querySelector("div").innerHTML = "Aula sobre manipulação do DOM";
+document.querySelector("#segunda").innerHTML = "Segunda-feira";
+document.querySelector(".terca").innerHTML = "Terça-feira";

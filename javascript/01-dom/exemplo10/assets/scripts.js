779983ab656ba00f9c9c71da9ae02a189fbd57ae
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-07 07:29:00 -0300

    Manipulação do DOM

--- javascript/01-dom/exemplo10/assets/scripts.js
+++
@@ -1,5 +0,0 @@
-var myLi =  document.createElement("li");
-myLi.textContent = "A";
-console.log(myLi);
-var myList = document.querySelector("#list");
-myList.insertAdjacentElement("afterbegin",myLi);

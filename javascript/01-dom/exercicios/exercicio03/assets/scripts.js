23c8c70a7e7d360b7b1d6f70dff399651b4ac582
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-12 08:02:13 -0300

    Exercício 03

--- javascript/01-dom/exercicios/exercicio03/assets/scripts.js
+++ javascript/01-dom/exercicios/exercicio03/assets/scripts.js
@@ -1,3 +1,10 @@
 
 
+// criação do
+const myTitle = document.createElement("h1");
+myTitle.textContent = "Disciplinas";
+myTitle.setAttribute("id","title");
+const body = document.querySelector("body");
+console.log(myTitle);
+body.insertAdjacentElement("afterbegin",myTitle);
 

b276970b9754940922642fedd70e9f1c778402f9
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-14 08:34:35 -0300

    Eventos

--- javascript/02-eventos/exemplo01/assets/scripts.js
+++ javascript/02-eventos/exemplo01/assets/scripts.js
@@ -1,9 +1,16 @@
 
 const buttonClick = document.querySelector("button");
-
+var count = 0;
 buttonClick.addEventListener("click", () => {
     buttonClick.textContent = "Fui clicado!";
+    count++;
+    console.log(count);
+    document.querySelector("div").textContent = `Contador: ${count}`;
 });
 buttonClick.addEventListener("mouseenter", () => {
     buttonClick.textContent = "Ponteiro do mouse entrou na DIV!";
+});
+
+buttonClick.addEventListener("mouseleave", () => {
+    buttonClick.textContent = "Ponteiro do mouse saiu na DIV!";
 });

dd0e5555c162820b3428f1c06314e127d3915ae5
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-02 13:37:03 -0300

    Estruturas - Exercícios

---
+++ php/02-variaveis/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require __DIR__ . '/../frame/lpwphp.php';
+
+LPWPHPClassName("02 - Variáveis");
+
+/**
+ * [tipos de dados] https://php.net/manual/pt_BR/language.types.php
+ * [ variáveis ] https://php.net/manual/pt_BR/language.variables.php
+ */
+LPWClassSession("variáveis", __LINE__);
+
+/**
+ * [ tipo boleano ] true | false
+ */
+LPWClassSession("tipo boleano", __LINE__);
+
+/**
+ * [ outros tipos ] string | array | objeto | numérico | null
+ */
+LPWClassSession("outros tipos", __LINE__);
---
+++ php/04-vetores/index.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/../frame/lpwphp.php';
+LPWPHPClassName("04 - Vetores");
+
+/**
+ * [ arrays ] https://php.net/manual/pt_BR/language.types.array.php
+ */
+LPWClassSession("index array", __LINE__);
+
+
+/**
+ * [ associative array ] "key" => "value"
+ */
+LPWClassSession("associative array", __LINE__);
+
+
+/**
+ * [ multidimensional array ] "key" => ["key" => "value"]
+ */
+LPWClassSession("multidimensional array", __LINE__);
+
+
+/**
+ * [ array access ] foreach(array as item) || foreach(array as key => value)
+ */
+LPWClassSession("array access", __LINE__);

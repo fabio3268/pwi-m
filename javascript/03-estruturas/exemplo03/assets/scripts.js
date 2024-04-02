bb9bad1ea8d9d2ba6a9442097326c46d13df5ff0
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-02 08:57:24 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo03/assets/scripts.js
+++ javascript/03-estruturas/exemplo03/assets/scripts.js
@@ -1,2 +1,18 @@
 
 
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for(var i = 0; i < numbers.length; i++){
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td>${i}</td><td>${numbers[i]}</td>`;
+    document.querySelector("#tableBody").appendChild(tr);
+
+}
+const searchButton = document.querySelector("#searchButton");
+searchButton.addEventListener("click", () => {
+    const searchInput = document.querySelector("#searchInput");
+    console.log(`Vou Procurar => ${searchInput.value}`);
+});
+

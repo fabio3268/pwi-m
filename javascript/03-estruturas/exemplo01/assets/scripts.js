9808a29d904c9e4cc5425f71fb76da7d27546767
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-02 08:31:02 -0300

    Estruturas - Exercícios

--- javascript/03-estruturas/exemplo01/assets/scripts.js
+++ javascript/03-estruturas/exemplo01/assets/scripts.js
@@ -1,6 +1,20 @@
 
 
+const teams = [
+    "Internacional",
+    "Grêmio",
+    "Juventude",
+    "Caxias",
+    "Brasil de Pelotas",
+    "Ipiranga",
+    "Avenida",
+    "Aimoré"
+];
 
-
-const teams = ["Internacional", "Grêmio", "Juventude", "Caxias", "Brasil de Pelotas"];
+for (var i = 0; i < teams.length; i++){
+    const divTeam = document.createElement("div");
+    divTeam.textContent = teams[i];
+    divTeam.setAttribute("class","teams");
+    document.querySelector("body").appendChild(divTeam);
+}
 
--- javascript/03-estruturas/exemplo02/assets/scripts.js
+++ javascript/03-estruturas/exemplo02/assets/scripts.js
@@ -4,3 +4,14 @@
 
 const numbers = [1,2,3,4,30,29,289,292,276];
 
+for (var i = 0; i < numbers.length; i++){
+    const divNumber = document.createElement("div");
+    divNumber.textContent = numbers[i];
+    if(numbers[i] % 2 == 0){
+        divNumber.setAttribute("class","even");
+        document.querySelector("#even").appendChild(divNumber);
+    } else {
+        divNumber.setAttribute("class","odd");
+        document.querySelector("#odd").appendChild(divNumber);
+    }
+}

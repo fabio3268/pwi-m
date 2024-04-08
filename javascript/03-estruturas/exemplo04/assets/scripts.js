508ffd07de1ab7c2202d1935d95a85e3e54bddfd
Fábio Santos <fabio3268@gmail.com>
2024-04-08 17:05:56 -0300

    Matrizes

--- javascript/03-estruturas/exemplo04/assets/scripts.js
+++ javascript/03-estruturas/exemplo04/assets/scripts.js
@@ -1,10 +1,18 @@
+var report = [[9,7,6.5],[9.8,9,8.5]];
+
+for(var c = 0; c <= 2; c++){
+    console.log(report[0][c]);
+}
+
+for(var c = 0; c <= 2; c++){
+    console.log(report[1][c]);
+}
 
 
+/*for(var c = 0; c <= 2; c++){
+    for(var l = 0; l <= 1; l++){
+        console.log(report[l][c]);
+    }
+}*/
 
 
-
-const arr = [10,20,30,40,50];
-console.table(arr);
-//console.table(arr.slice(2));
-//console.log(arr.slice(2,3));
-console.log(arr.slice(-2));

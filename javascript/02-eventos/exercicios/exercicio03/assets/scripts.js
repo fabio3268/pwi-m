053c09282c72d72ad16a45c5d26c42cac65f260c
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-09 08:49:49 -0300

    Estruturas - Exercícios

--- javascript/02-eventos/exercicios/exercicio03/assets/scripts.js
+++ javascript/02-eventos/exercicios/exercicio03/assets/scripts.js
@@ -1,4 +1,15 @@
 
 
+const myList = document.querySelector("#myList");
+
+myList.addEventListener("click", (element) => {
+    console.log(element.target.tagName);
+    if (element.target.tagName == "BUTTON"){
+        console.log("Apaga o item");
+        console.log(element.target.parentElement);
+        element.target.parentElement.remove();
+    }
+});
+
 
 
--- javascript/03-estruturas/exemplo04/assets/scripts.js
+++ javascript/03-estruturas/exemplo04/assets/scripts.js
@@ -1,18 +1,52 @@
-var report = [[9,7,6.5],[9.8,9,8.5]];
 
-for(var c = 0; c <= 2; c++){
+
+
+
+var report = [
+    [9,7,6.5],
+    [9.8,9,8.5]
+];
+
+//report[1][2] = 10;
+
+//console.log(report[1][2]);
+console.table(report);
+
+/*for(let c = 0; c <= 2; c++){
     console.log(report[0][c]);
 }
 
-/*for(var c = 0; c <= 2; c++){
+for(let c = 0; c <= 2; c++){
     console.log(report[1][c]);
 }*/
 
 
-/*for(var c = 0; c <= 2; c++){
-    for(var l = 0; l <= 1; l++){
+/*for(let c = 0; c <= 2; c++){
+    for(let l = 0; l <= 1; l++){
         console.log(report[l][c]);
     }
 }*/
 
+const rowFirst = document.querySelector("#first");
+const rowSecond = document.querySelector("#second");
 
+for(let c = 0; c <= 2; c++){
+    for (let l = 0; l <= 1; l++){
+        console.log(report[l][c]);
+        const td = document.createElement("td");
+        td.textContent = report[l][c]; // c=0 c=1 c=2
+        if(l == 0) {
+            rowFirst.append(td);
+        } else {
+            rowSecond.append(td);
+        }
+    }
+}
+
+/*
+for(let c = 0; c <= 2; c++){
+    console.log(report[1][c]);
+    const td = document.createElement("td");
+    td.textContent = report[1][c]; // c=0 c=1 c=2
+    rowSecond.append(td);
+}*/

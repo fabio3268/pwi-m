41b31f96e50b60f9d4a096aa878ef3526402d308
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-04-16 08:22:30 -0300

    readme.md - Intro

--- javascript/03-estruturas/exemplo05/assets/scripts.js
+++ javascript/03-estruturas/exemplo05/assets/scripts.js
@@ -1,6 +1,43 @@
 
 
+const employee = {
+    name : "Fábio Santos",
+    email : "fabiosantos@ifsul.edu.br",
+    password : "12345678"
+};
+/*console.table(employee);
+console.log(employee.name);
+console.log(employee.email);
+console.log(employee.password);*/
 
+const employee02 = employee;
+employee02.name = "João da Silva";
+//console.table(employee);
+/*
+employee.name = "João da Silva";
+console.table(employee02);
+*/
 
+/*const field = prompt("Selecione o campo de empregado: ");
+console.log(field);
 
+if(!employee[field]){
+    console.log("Esse campo não existe");
+} else {
+    console.log(`${field} : ${employee[field]}`);
+}*/
 
+console.log(Object.keys(employee));
+console.log(Object.values(employee));
+console.table(Object.entries(employee));
+
+let str = '';
+for (let key in employee) {
+    const value = employee[key];
+    console.log(key, value);
+    str += `<div>
+        <span>Chave: ${key}</span>
+        <span>Valor: ${value}</span>
+    </div>`;
+}
+document.body.innerHTML = str;
--- javascript/03-estruturas/exemplo06/assets/scripts.js
+++ javascript/03-estruturas/exemplo06/assets/scripts.js
@@ -1,28 +1,7 @@
 
 
-
-
-
-
-const pessoa = {
-    nome: 'Fulano',
-    sobrenome: 'Silva',
-    idade: 19,
-    salario: 2345.67,
+const employee = {
+    name : "Fábio Santos",
+    email : "fabiosantos@ifsul.edu.br",
+    password : "12345678"
 };
-
-//console.log(pessoa.nome);
-//onsole.log(pessoa.sobrenome);
-
-let str = '';
-for (let key in pessoa) {
-    console.log(key);
-    const value = pessoa[key];
-    console.log(value);
-    str += `<div>
-        <span>Chave: ${key}</span>
-        <span>Valor: ${value}</span>
-    </div>`;
-}
-console.log(str);
-document.body.innerHTML = str;

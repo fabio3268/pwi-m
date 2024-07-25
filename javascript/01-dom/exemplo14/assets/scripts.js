13346ab7ca4f6d524d2ce7db22af9bbf02592ebc
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-07-25 08:54:51 -0300

    Manipulando o DOM com JavaScript

--- javascript/01-dom/exemplo14/assets/scripts.js
+++ javascript/01-dom/exemplo14/assets/scripts.js
@@ -4,8 +4,29 @@ const plans = [
         price: 0,
         description: 'Funcionalidades básicas'
     },
-    { name: 'Pessoal', price: 9.99, description: 'Básico + Ferramentas avançadas' },
+    {
+        name: 'Pessoal',
+        price: 9.99,
+        description: 'Básico + Ferramentas avançadas'
+    },
     { name: 'Profissional', price: 19.99, description: 'Pessoal + Suporte dedicado' },
     { name: 'Empresarial', price: 39.99, description: 'Pro + Armazenamento ilimitado' },
 ];
 
+const divListBox = document.querySelectorAll(".box");
+console.log(divListBox);
+
+
+divListBox.forEach((element, index) => {
+    element.innerHTML = `
+        <div class="name">${plans[index].name}</div>
+        <div class="price">${plans[index].price}</div>
+        <div class="description">${plans[index].description}</div>
+    `;
+});
+
+divListBox.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+        console.log("oi");
+    });
+});
